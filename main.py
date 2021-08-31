215de216ae974cfa1bc5d18eeb3962182dbc8dd7
Amitay Drummer <amitay.drummer@gmail.com>
2021-08-31 23:28:06 +0300

    added a basic mock for demonstrating the concept

---
+++ main.py
@@ -0,0 +1,11 @@
+age = input("What is your age?")
+print("---")
+gender = input("What is your gender?")
+proffession = input("What is your proffesion?")
+core_neccesities = input(f"choose 3 out of the next 6 neccesities and rank them descendingly:\n"
+						 f"1. honor\n"
+						 f"2. money\n"
+						 f"3. meaning\n"
+						 f"4. reputation\n"
+						 f"5. health & well being\n"
+						 f"6. peace\n")
